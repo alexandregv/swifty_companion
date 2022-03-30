04ec6da2099ca743e1ea4ac18b8d716f67839f28
aguiot-- <aguiot--@student.42.fr>
2022-03-30 17:35:16 +0200

    Actually add AboutPage...

---
+++ lib/about.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+
+class AboutPage extends StatefulWidget {
+  const AboutPage({Key? key}) : super(key: key);
+
+  @override
+  _AboutPageState createState() => _AboutPageState();
+}
+
+class _AboutPageState extends State<AboutPage> {
+  PackageInfo _packageInfo = PackageInfo(
+    appName: 'Unknown',
+    packageName: 'Unknown',
+    version: 'Unknown',
+    buildNumber: 'Unknown',
+    buildSignature: 'Unknown',
+  );
+
+  Future<void> _initPackageInfo() async {
+    final info = await PackageInfo.fromPlatform();
+    setState(() {
+      _packageInfo = info;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _initPackageInfo();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('About'),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'Fluttery Companion',
+                style: TextStyle(fontSize: 40),
+              ),
+              Text(
+                'Version: ' + _packageInfo.version,
+                style: const TextStyle(fontSize: 20),
+              ),
+              const Text(
+                'by aguiot--',
+                style: TextStyle(fontSize: 20),
+              ),
+            ],
+          ),
+        )
+    );
+  }
+}
