a3841a3955b78366723acffbaab036366ef5462d
aguiot-- <aguiot--@student.42.fr>
2022-04-17 22:55:04 +0200

    Add basic user page with tabs and not found page

--- lib/users.dart
+++ lib/users.dart
@@ -15,10 +15,52 @@ class UsersPage extends StatefulWidget {
 class _UsersPageState extends State<UsersPage> {
   @override
   Widget build(BuildContext context) {
+    return buildUserNotFound(context);
+  }
+
+  Widget buildUserNotFound(BuildContext context) {
     return Scaffold(
+          appBar: AppBar(
+            title: Text(widget.login),
+            centerTitle: true,
+          ),
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.center, // Center vertically
+            crossAxisAlignment: CrossAxisAlignment.start, // Align to left
+            children: <Widget>[
+              Center(
+                child: Text("User ${widget.login} not found."),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                child: ElevatedButton.icon(
+                  onPressed: () => Navigator.pop(context),
+                  icon: const Icon(Icons.arrow_back),
+                  label: const Text('Go back'),
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size.fromHeight(40),
+                  ),
+                ),
+              ),
+            ],
+          )
+      );
+  }
+
+  Widget buildUserFound(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
         appBar: AppBar(
           title: Text(widget.login),
           centerTitle: true,
+          bottom: const TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.person)),
+              Tab(icon: Icon(Icons.commit)),
+              Tab(icon: Icon(Icons.area_chart)),
+            ],
+          ),
         ),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center, // Center vertically
@@ -30,7 +72,7 @@ class _UsersPageState extends State<UsersPage> {
             ),
           ],
         )
+      ),
     );
   }
-
 }
