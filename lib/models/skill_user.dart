2298a2864948db2fff7257e0d3b1b88875a54fcb
aguiot-- <aguiot--@student.42.fr>
2022-04-29 19:03:06 +0200

    Add SkillUser model

---
+++ lib/models/skill_user.dart
@@ -0,0 +1,21 @@
+class SkillUser {
+  final int skillId;
+  final String skillName;
+  final double level;
+
+  SkillUser({required this.skillId, required this.skillName, required this.level});
+
+  factory SkillUser.fromJson(Map<String, dynamic> json) {
+    SkillUser skillUser = SkillUser(
+      skillId: json["id"],
+      skillName: json["name"],
+      level: json["level"] ?? 0,
+    );
+    return skillUser;
+  }
+
+  @override
+  String toString() {
+    return 'SkillUser: $skillId $skillName $level';
+  }
+}
