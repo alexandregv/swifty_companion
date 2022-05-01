991ecefb18c3501d641522a7ff153ff3af950cd5
aguiot-- <aguiot--@student.42.fr>
2022-05-01 17:40:53 +0200

    Factorize user infos in tab1

--- lib/views/users/tab1.dart
+++ lib/views/users/tab1.dart
@@ -30,6 +30,32 @@ class _Tab1State extends State<Tab1> with AutomaticKeepAliveClientMixin<Tab1>{
   Widget build(BuildContext context) {
     super.build(context);
 
+    return SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            userImage(context),
+            ...userInfos(),
+          ],
+        )
+    );
+  }
+
+  Container userImage(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.fill,
+          image: widget.userImage,
+        ),
+      ),
+    );
+  }
+
+  List<Widget> userInfos() {
     final _level = widget.user.primaryCursus == null ? 'No cursus' : widget.user.primaryCursus?.level.toStringAsFixed(2);
     final _pool = () {
       if (widget.user.poolMonth == 'none' && widget.user.poolYear == 'none') {
@@ -43,57 +69,42 @@ class _Tab1State extends State<Tab1> with AutomaticKeepAliveClientMixin<Tab1>{
       }
     }();
 
-    return SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  fit: BoxFit.fill,
-                  image: widget.userImage,
-                ),
-              ),
-            ),
-            unnamedUserInfo("${widget.user.usualFirstName} ${widget.user.lastname.toUpperCase()}", style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-            if (widget.user.title.isNotEmpty) unnamedUserInfo(widget.user.title),
-            unnamedUserInfo(widget.user.email),
-            unnamedUserInfo(widget.user.location),
-            namedUserInfo('Lvl', _level!.toString()),
-            namedUserInfo('Wallet', widget.user.wallet.toString(), "₳"),
-            namedUserInfo('Evaluation points', widget.user.evaluationPoints.toString()),
-            namedUserInfo('Primary cursus', widget.user.primaryCursus?.cursus.name ?? 'No cursus'),
-            if (_pool != 'none') namedUserInfo('Pool', _pool),
-            if (widget.user.blackholedAt != null) namedUserInfo('Blackholed at', widget.user.blackholedAt!.toLocal().toString()),
-            if (widget.user.isStaff) namedUserInfo('Staff?', 'yes, panic'),
-          ],
-        )
-    );
-  }
+    Padding unnamedUserInfo(String value, {String? suffix = "", TextStyle? style = const TextStyle()}) {
+      return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+        child: Text(value + (suffix != null  && suffix.isNotEmpty ? " $suffix" : ""), style: style),
+      );
+    }
 
-  Padding unnamedUserInfo(String value, {String? suffix = "", TextStyle? style = const TextStyle()}) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-      child: Text(value + (suffix != null  && suffix.isNotEmpty ? " $suffix" : ""), style: style),
-    );
-  }
-  Padding namedUserInfo(String key, String value, [String? suffix]) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-      child: Text.rich(
-          TextSpan(
-              children: <TextSpan>[
-                TextSpan(text: key, style: const TextStyle(color: Colors.deepPurple)),
-                const TextSpan(text: " "),
-                TextSpan(text: value),
-                const TextSpan(text: " "),
-                TextSpan(text: suffix),
-              ]
-          )
-      ),
-    );
+    Padding namedUserInfo(String key, String value, [String? suffix]) {
+      return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+        child: Text.rich(
+            TextSpan(
+                children: <TextSpan>[
+                  TextSpan(text: key, style: const TextStyle(color: Colors.deepPurple)),
+                  const TextSpan(text: " "),
+                  TextSpan(text: value),
+                  const TextSpan(text: " "),
+                  TextSpan(text: suffix),
+                ]
+            )
+        ),
+      );
+    }
+
+    return [
+      unnamedUserInfo("${widget.user.usualFirstName} ${widget.user.lastname.toUpperCase()}", style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+      if (widget.user.title.isNotEmpty) unnamedUserInfo(widget.user.title),
+      unnamedUserInfo(widget.user.email),
+      unnamedUserInfo(widget.user.location),
+      namedUserInfo('Lvl', _level!.toString()),
+      namedUserInfo('Wallet', widget.user.wallet.toString(), "₳"),
+      namedUserInfo('Evaluation points', widget.user.evaluationPoints.toString()),
+      namedUserInfo('Primary cursus', widget.user.primaryCursus?.cursus.name ?? 'No cursus'),
+      if (_pool != 'none') namedUserInfo('Pool', _pool),
+      if (widget.user.blackholedAt != null) namedUserInfo('Blackholed at', widget.user.blackholedAt!.toLocal().toString()),
+      if (widget.user.isStaff) namedUserInfo('Staff?', 'yes, panic'),
+    ];
   }
 }
